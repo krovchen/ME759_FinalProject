4f5a75957856376cc9e23c3a00b556fdbea84366
krovchen <krovchen@Gmail.com>
2017-09-08 17:00:36 -0500

    Add files via upload

---
+++ HW01/problem1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	FILE *myin;
+	char fullID[256];
+	
+	myin = fopen("problem1.txt", "r");
+	char ch = fgetc(myin);
+	int i = 0;
+
+	while (ch != EOF) {
+		
+		fullID[i] = ch;
+		ch = getc(myin);
+		//printf("%c", ch);
+		i++;
+
+	}
+	//printf("%c \n", i);
+	printf("Hello! I'm student ");
+	
+	for(int j = 5; j > 1; j--){
+		printf("%c", fullID[i-j]);
+}	
+
+	printf("%c\r\n", '.');
+	printf("\r\n");
+
+
+	return 0;
+
+
+
+
+}
