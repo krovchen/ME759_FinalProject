cbf4036fcbfd626bed15555247baf6804a355836
krovchen <krovchen@Gmail.com>
2017-09-13 17:04:35 -0500

    Add files via upload

---
+++ problem3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	int d;
+	char c;
+	short s;
+	int *p;
+	int arr[2];
+	
+	p = &d;
+	*p = 10;
+	c = (char)1;
+	
+	printf("The size of integer is %lu\n", (unsigned long)sizeof(n));
+	
+	p = arr;
+	*(p+1) = 5;
+	p[0] = d;
+	*((char*)p + 1) = c;
+	
+	return 0;
+}
+	
