4c7c51d54ec3bf34a764b9863fe9ecc30668ff98
Kolya <krovchen@gmail.com>
2017-09-18 18:37:37 -0500

    gen_files.c

---
+++ HW02/gen_files.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+
+	
+	int numInts;
+	int i;
+	int j;
+	char fstr[] = "problem1_10.in";
+	char i_str[2];
+	//FILE *myOut = fopen("problem1.out", "w");
+	FILE *myin;
+
+	srand(0);
+	for(i = 10; i < 20; i++){	
+		numInts = 2^i;
+		//sprintf(i_str, "%d", 42);
+		//printf("c\n", (char)i);
+		
+		
+		//fstr = ["problem1_" itoa(i) ".out"];
+		myin = fopen(fstr, "w");
+		for(j = 0; j < numInts; j++){
+			fprintf(myin, "%d \n", rand());
+		}
+		fstr[10] = fstr[10]+1;
+	}
+
+	return 0;
+}
+
+
+
+
