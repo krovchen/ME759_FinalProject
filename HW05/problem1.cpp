27f926f5f8b1716254f95567742eb3c51b3912fb
Kolya <krovchen@gmail.com>
2017-10-06 18:54:27 -0500

    m

--- HW05/problem1.cpp
+++ HW05/problem1.cpp
@@ -77,7 +77,7 @@ int main(int argc, char *argv[]) {
 	clock_t t2 = clock();
 	if(j == 0) mintime = float(t2-t1);
 	else mintime = min(float(t2-t1), mintime);
-	//cout << float(t2-t1)/CLOCKS_PER_SEC*1000 << endl;
+	cout << float(t2-t1)/CLOCKS_PER_SEC*1000 << endl;
 	//cout << mintime << endl;
 }
 
