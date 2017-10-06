13f0c40b9845525d2143427c80c0102649aa4f9f
Kolya <krovchen@gmail.com>
2017-10-06 18:22:51 -0500

    ans

--- HW05/problem1.cpp
+++ HW05/problem1.cpp
@@ -1,8 +1,9 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include "stopwatch.hpp"
 #include <omp.h>
+#include "math.h"
+#include <chrono>
 
 using namespace std;
 
@@ -10,11 +11,17 @@ using namespace std;
 #define ncols 1200
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	size_t size = nrows*ncols;
 	int i;
 	int temp;
+
+
+	int ncounts[7] = {0,0,0,0,0,0, 0};
+
+	int nthreads = atoi(argv[1]);
+	
 	int num0 = 0;
 	int num1 = 0;
 	int num2 = 0;
@@ -23,31 +30,41 @@ int main() {
 	int num5 = 0;
 	int num6 = 0;
 
+	
 	vector<int> in_vec(size);
+
 	ifstream file_in;
 	file_in.open("picture.inp");
 
 	for(i = 0; i < size; i++)
 	{
 		file_in >> temp;
-		in_vec[i] = temp;
-		
+		in_vec[i] =temp;
 
 	}
 
-	#pragma omp parallel num_threads(5)
-	{
-	for(i = 0; i < size; i++)
-	{
-		if(in_vec[i] == 0) num0++;
-		if(in_vec[i] == 1) num1++;
-		if(in_vec[i] == 2) num2++;
-		if(in_vec[i] == 3) num3++;
-		if(in_vec[i] == 4) num4++;
-		if(in_vec[i] == 5) num5++;
-		if(in_vec[i] == 6) num6++;
-	}
-	}
+	auto t1 = std::chrono::high_resolution_clock::now();
+
+	#pragma omp parallel num_threads(nthreads) 
+{
+	#pragma omp for reduction(+:num0, num1, num2, num3, num4, num5, num6)
+	
+	
+	
+		for(i = 0; i < size; i++)
+		{
+			if(in_vec[i] == 0) num0++;
+			if(in_vec[i] == 1) num1++;
+			if(in_vec[i] == 2) num2++;
+			if(in_vec[i] == 3) num3++;
+			if(in_vec[i] == 4) num4++;
+			if(in_vec[i] == 5) num5++;
+			if(in_vec[i] == 6) num6++;
+		}
+	
+}
+	auto t2 = std::chrono::high_resolution_clock::now();
+
 	cout << num0 << endl;
 	cout << num1 << endl;
 	cout << num2 << endl;
@@ -57,6 +74,7 @@ int main() {
 	cout << num6 << endl;
 	cout << num0+num1+num2+num3+num4+num5+num6 << endl;
 	cout << size << endl;
+	cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << endl;
 
 
 }
