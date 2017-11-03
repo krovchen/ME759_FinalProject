083ccc735718359893f79c9e246c749bb15dabd5
krovchen <mkravchenko@wisc.edu>
2017-11-03 18:34:02 -0500

    done

--- HW09/problem1b.cu
+++ HW09/problem1b.cu
@@ -47,8 +47,107 @@ void applyStencil1D_SEQ(int sIdx, int eIdx, const float *weights, float *in, flo
 }
 
 __global__ void applyStencil1D(int sIdx, int eIdx, const float *weights, float *in, float *out) {
-    int i = sIdx + blockIdx.x*blockDim.x + threadIdx.x;
-    if( i < eIdx ) {
+   // int i = sIdx + blockIdx.x*blockDim.x + threadIdx.x;
+    __shared__ volatile float sharedInput[512];
+
+
+    int sInd = threadIdx.x;
+    int bInd = threadIdx.x+blockIdx.x*blockDim.x;
+
+    if(bInd < sIdx+eIdx)
+    sharedInput[sInd] = in[bInd];
+
+    __syncthreads();
+    __threadfence_block();
+
+
+if((bInd-sIdx) < eIdx){
+		    
+		float result = 0.f;
+		if(sInd < 509 && sInd > 2){
+		 	
+       			result += weights[0]*sharedInput[sInd-3];
+        		result += weights[1]*sharedInput[sInd-2];
+        		result += weights[2]*sharedInput[sInd-1];
+        		result += weights[3]*sharedInput[sInd];
+       	 		result += weights[4]*sharedInput[sInd+1];
+        		result += weights[5]*sharedInput[sInd+2];
+        		result += weights[6]*sharedInput[sInd+3];
+			result /=7.f;
+   			out[bInd] = result;
+			}
+		if(sInd == 509){
+       		 	result += weights[0]*sharedInput[sInd-3];
+        		result += weights[1]*sharedInput[sInd-2];
+        		result += weights[2]*sharedInput[sInd-1];
+        		result += weights[3]*sharedInput[sInd];
+       	 		result += weights[4]*sharedInput[sInd+1];
+        		result += weights[5]*sharedInput[sInd+2];
+        		result += weights[6]*in[bInd+3];
+			result /=7.f;
+   			out[bInd] = result;
+			}
+		if(sInd == 510){
+	       		 result += weights[0]*sharedInput[sInd-3];
+        		result += weights[1]*sharedInput[sInd-2];
+        		result += weights[2]*sharedInput[sInd-1];
+        		result += weights[3]*sharedInput[sInd];
+       	 		result += weights[4]*sharedInput[sInd+1];
+        		result += weights[5]*in[bInd+2];
+        		result += weights[6]*in[bInd+3];
+			result /=7.f;
+   			out[bInd] = result;
+			}
+		if(sInd == 511){
+			result += weights[0]*sharedInput[sInd-3];
+        		result += weights[1]*sharedInput[sInd-2];
+        		result += weights[2]*sharedInput[sInd-1];
+        		result += weights[3]*sharedInput[sInd];
+       	 		result += weights[4]*in[bInd+1];
+        		result += weights[5]*in[bInd+2];
+        		result += weights[6]*in[bInd+3];
+			result /=7.f;
+   			out[bInd] = result;
+			}	
+		if(blockIdx.x > 0){
+			if(sInd == 2){
+				result += weights[0]*in[bInd-3];
+        			result += weights[1]*sharedInput[sInd-2];
+        			result += weights[2]*sharedInput[sInd-1];
+        			result += weights[3]*sharedInput[sInd];
+       	 			result += weights[4]*sharedInput[sInd+1];
+        			result += weights[5]*sharedInput[sInd+2];
+        			result += weights[6]*sharedInput[sInd+3];
+				result /=7.f;
+   				out[bInd] = result;
+				}
+			if(sInd == 1){				
+				result += weights[0]*in[bInd-3];
+        			result += weights[1]*in[bInd-2];
+        			result += weights[2]*sharedInput[sInd-1];
+        			result += weights[3]*sharedInput[sInd];
+       	 			result += weights[4]*sharedInput[sInd+1];
+        			result += weights[5]*sharedInput[sInd+2];
+        			result += weights[6]*sharedInput[sInd+3];
+				result /=7.f;
+   				out[bInd] = result;
+				}
+			if(sInd == 0){
+				result += weights[0]*in[bInd-3];
+        			result += weights[1]*in[bInd-2];
+        			result += weights[2]*in[bInd-1];
+        			result += weights[3]*sharedInput[sInd];
+       	 			result += weights[4]*sharedInput[sInd+1];
+        			result += weights[5]*sharedInput[sInd+2];
+        			result += weights[6]*sharedInput[sInd+3];
+				result /=7.f;
+   				out[bInd] = result;
+				}
+		}
+
+		
+   }
+   /* if( i < eIdx ) {
         float result = 0.f;
         result += weights[0]*in[i-3];
         result += weights[1]*in[i-2];
@@ -59,9 +158,8 @@ __global__ void applyStencil1D(int sIdx, int eIdx, const float *weights, float *
         result += weights[6]*in[i+3];
         result /=7.f;
         out[i] = result;
-    }
+    }*/
 }
-
 int main(int argc, char* argv[]) {
   if(argc!=2){
         printf("Usage %s N\n",argv[0]);
@@ -85,10 +183,22 @@ int main(int argc, char* argv[]) {
   float *d_in;       cudaMalloc(&d_in, size);
   float *d_out;      cudaMalloc(&d_out, size);
   
+		cudaEvent_t startEvent_inc;
+	cudaEvent_t stopEvent_inc;
+	cudaEventCreate(&startEvent_inc);
+	cudaEventCreate(&stopEvent_inc);
+	cudaEventRecord(startEvent_inc,0);
+
+
   cudaMemcpy(d_weights,weights,wsize,cudaMemcpyHostToDevice);
   cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice);
-  applyStencil1D<<<(N+511)/512, 512>>>(RADIUS, N-RADIUS, d_weights, d_in, d_out);
+  applyStencil1D<<<(N+511)/512, 512, 512*sizeof(float)>>>(RADIUS, N-RADIUS, d_weights, d_in, d_out);
   cudaMemcpy(cuda_out, d_out, size, cudaMemcpyDeviceToHost);
+
+
+cudaEventRecord(stopEvent_inc,0);  //ending timing for inclusive
+	cudaEventSynchronize(stopEvent_inc);   
+	cudaEventElapsedTime(&time, startEvent_inc, stopEvent_inc);
 
   applyStencil1D_SEQ(RADIUS, N-RADIUS, weights, in, out);
   int nDiffs = checkResults(RADIUS, N-RADIUS, cuda_out, out);
