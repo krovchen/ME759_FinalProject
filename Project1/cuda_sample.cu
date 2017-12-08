a0493233dd9de00347cdde403335f4cfca4bc506
Mykola <mkravchenko@wisc.edu>
2017-12-07 21:59:54 -0600

    cuda inter device kernel comms testing

--- Project1/cuda_sample.cu
+++ Project1/cuda_sample.cu
@@ -70,12 +70,12 @@ int main()
 
 		
 	cout << "Cuda Error: " << cErr << endl;	
-	bool k_stop_cmd = 1;
-	bool *host_stop_kernel = &k_stop_cmd;
-	cudaStream_t stream1;
-	cudaStreamCreate(&stream1);
-	cout <<"Trying to Stop Helper Kernel" << endl;
-	cudaMemcpy(&stop_kernel, host_stop_kernel, sizeof(bool), cudaMemcpyHostToDevice);
+	//bool k_stop_cmd = 1;
+	//bool *host_stop_kernel = &k_stop_cmd;
+	//cudaStream_t stream1;
+	//cudaStreamCreate(&stream1);
+	//cout <<"Trying to Stop Helper Kernel" << endl;
+	//cudaMemcpy(&stop_kernel, host_stop_kernel, sizeof(bool), cudaMemcpyHostToDevice);
 	//cudaStreamSynchronize(stream1);
 	dataKernel<<<1, 1>>>(dVal);
 	cudaMemcpy(h_data, dVal, sizeof(int), cudaMemcpyDeviceToHost);
