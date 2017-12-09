929072124a1f859939513a426945516cc457f999
Mykola <mkravchenko@wisc.edu>
2017-12-09 15:29:53 -0600

    first testing w/ comms

--- Project1/main_fcn.cu
+++ Project1/main_fcn.cu
@@ -50,7 +50,7 @@ bool help_fcn(help_input_from_main help_input, double* out);
 bool init_help(help_input_from_main help_input);
 
 //function declarations -- calc kernel and monitor kernel
-__global__ void dataKernel( int* data, int size, int* held_Data, bool *stop_kern_ptr);
+__global__ void dataKernel( int* data,  bool *stop_kern_ptr);
 __global__ void monitorKernel(int * write_2_ptr,  int * read_in_ptr);
 
 
@@ -85,10 +85,10 @@ int main()
 
 		if(omp_get_thread_num() == 1){
 			cout <<"Running CUDA init" << endl;
-			const int numElems = 4;
+			const int numElems = 1;
 			int hostArray[numElems];
 			int *dArray;
-			int *dArray_Held;
+
 			int i = 0;
 
 			//pointer of helper function return	
@@ -107,9 +107,9 @@ int main()
 			cudaGetSymbolAddress((void**)&stop_kern_ptr, stop_kernel);
 
 			cudaMalloc((void**)&dArray, sizeof(int)*numElems);
-			cudaMalloc((void**)&dArray_Held, sizeof(int)*numElems);
+			//cudaMalloc((void**)&dArray_Held, sizeof(int)*numElems);
 			cudaMemset(dArray, 0, numElems*sizeof(int));
-			cudaMemset(dArray_Held, 0, numElems*sizeof(int));
+			//cudaMemset(dArray_Held, 0, numElems*sizeof(int));
 			cudaMallocHost((void**)&monitor_data, sizeof(int));
 			cudaStream_t stream1;
 			cudaStreamCreateWithFlags(&stream1, cudaStreamNonBlocking);
@@ -122,11 +122,11 @@ int main()
 					CF.call_help_cmd = 0;
 					cout <<"Launching Helper Kernel" << endl;
 					//*help_rdy =  help_fcn(*help_input, out);
-					dataKernel<<<1, 4>>>(dArray, numElems, dArray_Held, stop_kern_ptr);
+					dataKernel<<<1, 4>>>(dArray, stop_kern_ptr);
 				}
 				if(CF.help_running_cmd == 1 && allow_interrupt == 0 && CF.request_val_cmd == 1){	
 					cout <<"Launching Monitor Kernel" << endl;
-					cudaStreamSynchronize(stream1);
+					//cudaStreamSynchronize(stream1);
 					monitorKernel<<<1, 1,0, stream1>>>(monitor_data, dArray);
 					cout <<"Launching Async Mem Cpy" << endl;
 					cudaMemcpyAsync(h_data, monitor_data, sizeof(int), cudaMemcpyDeviceToHost, stream1);
@@ -143,8 +143,8 @@ int main()
 			cudaMemcpyAsync(stop_kern_ptr, host_stop_kernel, sizeof(bool), cudaMemcpyHostToDevice, stream1);
 			cudaStreamSynchronize(stream1);
 
-			cout << "Copying values from helper kernel to base (but they may be garbage!!!!!" << endl;
-			cudaMemcpy(&hostArray, dArray_Held, sizeof(int)*numElems, cudaMemcpyDeviceToHost);
+			//cout << "Copying values from helper kernel to base (but they may be garbage!!!!!" << endl;
+			//cudaMemcpy(&hostArray, dArray_Held, sizeof(int)*numElems, cudaMemcpyDeviceToHost);
 
 
 			for(i = 0; i < numElems; i++)
@@ -259,9 +259,28 @@ return 1;
 
 
 
-__global__ void dataKernel( int* data, int size, int* data_held, bool *stop_kern_ptr){
+__global__ void dataKernel( int* data, bool *stop){
 //this adds a value to a variable stored in global memory
-	int thid = threadIdx.x+blockIdx.x*blockDim.x;
+	*data = 3;
+
+	while(1){
+		if(*data > 300)
+			*data = 0;
+		*data = *data+1;
+		if(*stop == 1){
+			*data = 6;
+			__syncthreads();
+			asm("trap;");
+		}
+
+		
+	}	
+
+
+
+
+
+/* int thid = threadIdx.x+blockIdx.x*blockDim.x;
 
 	if(thid < size){
 		data_held[thid] = (blockIdx.x+ threadIdx.x);
@@ -280,7 +299,7 @@ __global__ void dataKernel( int* data, int size, int* data_held, bool *stop_kern
 
 
 		}
-	}
+	}*/
 
 }
 
