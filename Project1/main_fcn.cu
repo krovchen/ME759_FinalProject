8a48630b62ac73566d79a51efb60eac92d73dc67
Mykola <mkravchenko@wisc.edu>
2017-12-10 17:36:39 -0600

    not doing test case

--- Project1/main_fcn.cu
+++ Project1/main_fcn.cu
@@ -253,7 +253,7 @@ __global__ void dataKernel( double* data, int nsteps){
 		while(wait == 1){
 			now = clock();
 			clock_t cycles = now > start ? now - start : now + (0xffffffff - start);
-			if(cycles > 500000)
+			if(cycles > 50000)
 				wait = 0;
 		}		
 		wait = 1;
