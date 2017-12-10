a68ca526c041e4678f28cb35851fbffc7fd10976
Mykola <mkravchenko@wisc.edu>
2017-12-10 16:47:59 -0600

    more device function

--- Project1/test_case_test.cu
+++ Project1/test_case_test.cu
@@ -146,7 +146,7 @@ int main(int argc, char** argv)
 
 
 	dataKernel<<<dimGrid, dimBlock, sizeof(double)*TileSize*TileSize*TileSize*TileSize>>>(dC, dA, dB, nSteps, temp1, temp2, temp3);
-	sleep(.1);
+	sleep(.0001);
 		cout <<"Launching Monitor Kernel" << endl;
 	monitorKernel<<<1, 1,0, stream1>>>(monitor_data, &dC[1]);
 	cout <<"Launching Async Mem Cpy" << endl;
@@ -154,7 +154,7 @@ int main(int argc, char** argv)
 
 	cudaStreamSynchronize(stream1);
 	cout << "Value monitored over: "  << *h_data*100 << endl;
-	sleep(.001);
+	sleep(.0001);
 		cout <<"Launching Monitor Kernel" << endl;
 	monitorKernel<<<1, 1,0, stream1>>>(monitor_data, &dC[1]);
 	cout <<"Launching Async Mem Cpy" << endl;
