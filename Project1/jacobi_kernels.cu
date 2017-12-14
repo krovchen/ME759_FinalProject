2016078fb6e36d30835de4641954e1fac1d88c87
Mykola <mkravchenko@wisc.edu>
2017-12-13 23:49:22 -0600

    splitting things up into h files

---
+++ Project1/jacobi_kernels.cu
@@ -0,0 +1,140 @@
+#include<cuda.h>
+#include<iostream>
+#include<stdio.h>
+#include "omp.h"
+#include <unistd.h>
+#include <sys/time.h>
+#include "test_kernels.h"
+#include "main_fcn.h"
+
+
+using namespace std;
+
+
+__global__ void monitorKernel(double * write_2_ptr,  double * read_in_ptr){
+
+	int thid = threadIdx.x+blockIdx.x*blockDim.x;
+	write_2_ptr[thid] = read_in_ptr[thid];
+
+
+}
+
+
+
+
+
+__global__ void dataKernel( double* data, int nsteps){
+//this adds a value to a variable stored in global memory
+	int thid = threadIdx.x+blockIdx.x*blockDim.x;
+	//data[thid] = 0;
+	int i = 0;
+	//bool wait = 1;
+
+	//clock_t start = clock64();
+	//clock_t now;
+
+	while(i < nsteps){
+		data[thid] = data[thid]+.1*thid;
+		i=i+1;
+	}
+
+	/*	clock_t start = clock64();
+		i = i+1;
+		while(wait == 1){
+			now = clock();
+			clock_t cycles = now > start ? now - start : now + (0xffffffff - start);
+			if(cycles > 5000)
+				wait = 0;
+		}		
+		wait = 1;
+		
+	}	*/
+
+
+
+}
+
+
+bool help_fcn(help_input_from_main help_input, double* out){
+	//int j = 1;
+	int i = 0;
+	double* inp1 = help_input.inp1;
+	double* inp2 = help_input.inp1;
+	
+	for(i = 0; i < N; i++){
+
+		if(i > 0)
+			//*out = (*out+inp1[i]+inp2[i])*i/(i+1);
+			*out = (*out+inp1[i]+inp2[i]);
+		else
+			//*out = (*out+inp1[i]+inp2[i]);
+			*out = (*out+inp1[i]+inp2[i]);	
+		//cout << "out after update = " << *out << endl;	
+
+	
+	}
+	return 1;
+}
+
+
+
+bool main_fcn(ctrl_flags CF, double* help_out, help_input_from_main* help_input_ptr)
+{	
+	struct timeval stop, start;
+	bool *call_help = CF.call_help;
+	//volatile bool *help_rdy = CF.help_rdy;
+	volatile bool *request_val = CF.request_val;
+	volatile bool *request_done = CF.request_done;
+
+	//initialize data for input to helper function
+	double inp1[N] = {4};
+	int i = 0;
+	int numReads = 10;
+	double sval;
+	double sum_times = 0;
+	int j = 0;
+
+	//set values of helper function input
+	(*help_input_ptr).initS(inp1);
+	//ask to start help function	
+	cout << "Main calling help function for 1st time" << endl;
+	*call_help = 1;
+	
+	//=====USER CODE before calling help GOES HERE==========
+	sleep(.01);
+
+
+	for(j = 0; j < numReads; j++){
+	gettimeofday(&start, NULL);
+
+		//BELOW IS WHERE YOU CALL THE HELPER READ FROM MAIN
+		*request_val = 1;
+		while(*request_done == 0)
+			sleep(.00000001);
+		//ABOVE IS WHERE YOU CALL THE HELPER READ FROM MAIN -- now help value(s) is in *help_out
+
+	gettimeofday(&stop, NULL);
+	sval = (stop.tv_sec-start.tv_sec)*1000000; //sec to us
+	sval = sval + stop.tv_usec-start.tv_usec; //us
+
+	cout << "Time between message request and message receive in us is: " << sval << endl;
+	for(i = 0; i < 3; i++)
+		cout << "Main update received " << help_out[i] << endl;
+	*request_done = 0;
+	if(j > 0)  //skip the first call because its bad fro some reason
+		sum_times = sum_times+sval;
+	sleep(.2);
+
+	}
+
+	
+	cout << "Average read time between message request and message received in us is: " << sum_times/(numReads-1) << endl;
+
+	//=======USER code AFTER calling helper goes here======
+
+	cout << "Exiting Main" << endl;
+	
+	return 1;
+
+
+}
---
+++ Project1/test_kernels.h
@@ -0,0 +1,80 @@
+/*
+* Copyright 1993-2006 NVIDIA Corporation.  All rights reserved.
+*
+* NOTICE TO USER:   
+*
+* This source code is subject to NVIDIA ownership rights under U.S. and 
+* international Copyright laws.  
+*
+* This software and the information contained herein is PROPRIETARY and 
+* CONFIDENTIAL to NVIDIA and is being provided under the terms and 
+* conditions of a Non-Disclosure Agreement.  Any reproduction or 
+* disclosure to any third party without the express written consent of 
+* NVIDIA is prohibited.     
+*
+* NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE 
+* CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR 
+* IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH 
+* REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF 
+* MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.   
+* IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL, 
+* OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS 
+* OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE 
+* OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE 
+* OR PERFORMANCE OF THIS SOURCE CODE.  
+*
+* U.S. Government End Users.  This source code is a "commercial item" as 
+* that term is defined at 48 C.F.R. 2.101 (OCT 1995), consisting  of 
+* "commercial computer software" and "commercial computer software 
+* documentation" as such terms are used in 48 C.F.R. 12.212 (SEPT 1995) 
+* and is provided to the U.S. Government only as a commercial end item.  
+* Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through 
+* 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the 
+* source code with only those rights set forth herein.
+*/
+
+
+
+
+
+#ifndef _TESTKERNEL_H_
+#define _TESTKERNEL_H_
+
+
+#include<cuda.h>
+#include<iostream>
+#include<stdio.h>
+#include "omp.h"
+#include <unistd.h>
+#include <sys/time.h>
+#include "main_fcn.h"
+
+//global variables
+const bool allow_interrupt = 0;
+const int N = 1;
+const int numElems =65536;
+
+struct help_input_from_main{
+	static const int length = N;
+	double inp1[N];
+
+	void initS(double* v1){
+		int i = 0;
+		for(i = 0; i < N; i++){
+			inp1[i] = v1[i];
+		}
+	}
+
+};
+
+
+bool main_fcn(ctrl_flags CF, double* help_out, help_input_from_main* help_input_ptr);
+bool help_fcn(help_input_from_main help_input, double* out);
+__global__ void dataKernel( double* data, int nsteps);
+__global__ void monitorKernel(double * write_2_ptr,  double * read_in_ptr);
+
+
+
+
+#endif // _TESTKERNEL_H_
+
