73fd441164b881e3fdbecb50ab0298b64b11d495
Mykola <mkravchenko@wisc.edu>
2017-12-18 17:00:47 -0600

    last commit maybe

--- Project1/main_fcn.cu
+++ Project1/main_fcn.cu
@@ -105,7 +105,7 @@ int main()
 					monitorKernel<<<numBlocks, numThreads,0, stream1>>>(monitor_data, dArray);
 					cout <<"Launching Async Mem Cpy" << endl;
 					cudaMemcpyAsync(h_data, monitor_data, numElems*sizeof(double), cudaMemcpyDeviceToHost, stream1);
-					cudaStreamSynchronize(stream1);
+					//cudaStreamSynchronize(stream1);
 					CF.request_val_cmd = 0;
 					for(i = 0; i < numElems; i++){
 						
